3381c7ee4eb89d0b87f1da9219974d9ae1a68a5b
ddhineshh <dhineshtd@gmail.com>
2023-01-07 13:48:34 +0530

    react second

---
+++ src/Carditem.js
@@ -0,0 +1,47 @@
+const ProductData = [
+    {
+        id: 1,
+        title: 'Sport Product',
+        price: '$400',
+        rating: 4,
+        cart: false
+    },
+    {
+        id: 2,
+        title: 'Fashion Product',
+        price: '$300',
+        rating: 5,
+        cart: false
+    },
+    {
+        id: 3,
+        title: 'Sneakers',
+        price: '$350',
+        rating: 4,
+        cart:false
+    },
+    {
+        id:4,
+        title: 'Sweatshirts',
+        price: '$500',
+        rating:5,
+        cart:false
+    },
+    {
+        id:5,
+        title: 'Jewelerry',
+        price: '$1000',
+        rating: 4,
+        cart:false
+    },
+    {
+        id:6,
+        title: 'Keyboard',
+        price: '$150',
+        rating: 4,
+        cart: false
+
+    }
+]
+
+export default ProductData;
